9db31a5f83a97f0c7a088a9ebde231b95cfac4b3
mengmanzbh <1934068305@qq.com>
2016-09-07 10:59:40 +0800

    OK
    OK

--- Mybatis_A/src/main/java/Mybatis_A/Mybatis_A/App.java
+++ Mybatis_A/src/main/java/Mybatis_A/Mybatis_A/App.java
@@ -1,6 +1,7 @@
 package Mybatis_A.Mybatis_A;
 
 import java.io.InputStream;
+import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -10,30 +11,49 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	   //mybatis的配置文件
-        String resource = "conf.xml";
-        //使用类加载器加载mybatis的配置文件（它也加载关联的映射文件）
-        InputStream is = App.class.getClassLoader().getResourceAsStream(resource);
-        //构建sqlSession的工厂
-        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
-        //使用MyBatis提供的Resources类加载mybatis的配置文件（它也加载关联的映射文件）
-        //Reader reader = Resources.getResourceAsReader(resource); 
-        //构建sqlSession的工厂
-        //SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
-        //创建能执行映射文件中sql的sqlSession
-        SqlSession session = sessionFactory.openSession();
-        /**
-         * 映射sql的标识字符串，
-         * me.gacl.mapping.userMapper是userMapper.xml文件中mapper标签的namespace属性的值，
-         * getUser是select标签的id属性值，通过select标签的id属性值就可以找到要执行的SQL
-         */
-        String statement = "me.gacl.mapping.userMapper.getUser";//映射sql的标识字符串
-        //执行查询返回一个唯一user对象的sql
-        User user = session.selectOne(statement, 2);
-        System.out.println(user);
-    }
+public class App {
+	public static void main(String[] args) {
+		String resource = "conf.xml";
+		InputStream is = App.class.getClassLoader().getResourceAsStream(resource);
+		SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
+		SqlSession session = sessionFactory.openSession();
+		// 查询一条
+		// String statement = "me.gacl.mapping.userMapper.getUser";//
+		// 映射sql的标识字符串
+		// User user = session.selectOne(statement, 2);
+		// System.out.println(user);
+
+		// 查询全部
+		String statement = "me.gacl.mapping.userMapper.getAll";// 映射sql的标识字符串
+		List<User> list = session.selectList(statement);
+		System.out.println(list);
+
+		// 添加
+		// User userA = new User();
+		// userA.setId(3);
+		// userA.setAge(32);
+		// userA.setName("王菲");
+		// String statement = "me.gacl.mapping.userMapper.insertAuthor";//
+		// 映射sql的标识字符串
+		// int result = session.insert(statement, userA);
+		// session.commit();
+		// System.out.println(result);
+
+		// 删除
+		// String statement =
+		// "me.gacl.mapping.userMapper.deleteUserById";//映射sql的标识字符串
+		// session.delete(statement, 1); session.commit();
+
+		// 更新数据
+		// User userA = new User();
+		// userA.setId(15);
+		// userA.setAge(32);
+		// userA.setName("王菲111");
+		// String statement = "me.gacl.mapping.userMapper.updateOne";//
+		// 映射sql的标识字符串
+		// int result = session.update(statement, userA);
+		// session.commit();
+		// System.out.println(result);
+
+	}
 }
